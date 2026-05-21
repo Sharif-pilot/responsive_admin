a44da2394cdfeff1d9946e6b1cfd50c54c2d4444
Mohammad Sharif Hussaini <34144696+Sharif-pilot@users.noreply.github.com>
2026-05-21 10:24:32 +0430

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,8 @@ class MyApp extends StatelessWidget {
         '/permissions': (context) => PermissionScreen(),
         '/roles': (context) => RoleScreen(),
         '/users': (context) => UserListScreen(),
-        '/settings': (context) => SettingsScreen(), 
+        '/settings': (context) => SettingsScreen(),
+        
       },
     );
   }
